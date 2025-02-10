b3524a39325a25c1ab38b0e554a4df95c5497e50
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-02-10 16:49:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstWord(s) {
   // your code here
+	let rms=s.trim();
+	let rms=s.split(" ");
+	return word[0];
 }
-
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
+
+
+
+
+
+
